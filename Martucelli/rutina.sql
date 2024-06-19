f15ae931305f02f13b31f5fdb47f9eef3b4650e9
Franco Martucelli <138329617+MartucelliF@users.noreply.github.com>
2024-06-19 20:26:06 -0300

    Update rutina.sql

--- Martucelli/rutina.sql
+++ Martucelli/rutina.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-06-2024 a las 03:07:08
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
+-- Tiempo de generación: 20-06-2024 a las 01:25:20
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,21 @@ SET time_zone = "+00:00";
 CREATE TABLE `categorias` (
   `id_categoria` int(10) NOT NULL,
   `nombre_categoria` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id_categoria`, `nombre_categoria`) VALUES
+(1, 'Matemática'),
+(2, 'Lengua y Literatura'),
+(3, 'Programación Web'),
+(4, 'GYM'),
+(5, 'Inglés'),
+(6, 'Organización de las computadoras'),
+(7, 'UBA'),
+(8, '¡Abrulandia!');
 
 -- --------------------------------------------------------
 
@@ -42,7 +56,7 @@ CREATE TABLE `preguntas` (
   `id_pregunta` int(10) NOT NULL,
   `id_subcategoria` int(10) NOT NULL,
   `pregunta` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -54,7 +68,7 @@ CREATE TABLE `respuestas` (
   `id_respuesta` int(10) NOT NULL,
   `id_pregunta` int(10) NOT NULL,
   `respuesta` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -73,19 +87,6 @@ CREATE TABLE `rutinaview` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `rutina_asd`
---
-
-CREATE TABLE `rutina_asd` (
-  `id_usuario` int(10) NOT NULL,
-  `num_tarea` int(10) DEFAULT NULL,
-  `turno_tarea` char(10) DEFAULT NULL,
-  `categoría_tarea` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `subcategorias`
 --
 
@@ -93,7 +94,38 @@ CREATE TABLE `subcategorias` (
   `id_subcategoria` int(10) NOT NULL,
   `id_categoria` int(10) NOT NULL,
   `nombre_subcategoria` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `subcategorias`
+--
+
+INSERT INTO `subcategorias` (`id_subcategoria`, `id_categoria`, `nombre_subcategoria`) VALUES
+(1, 1, 'Funciones'),
+(2, 1, 'Fracciones'),
+(3, 1, 'Matrices'),
+(4, 2, 'Género literario'),
+(5, 2, 'Comprensión lectora'),
+(6, 3, 'HTML'),
+(7, 3, 'CSS'),
+(8, 3, 'JavaScript'),
+(9, 3, 'Bootstrap'),
+(10, 4, 'Tren inferior'),
+(11, 4, 'Tren superior'),
+(12, 5, 'Verb To Be'),
+(13, 5, 'Verbal Times'),
+(14, 6, 'Componentes'),
+(15, 6, 'Sistemas Operativos'),
+(16, 6, 'Máquinas Virtuales'),
+(17, 7, 'Psicología'),
+(18, 7, 'Derecho'),
+(19, 8, 'Estudiar Freud'),
+(20, 8, 'Pasear a Lari'),
+(21, 8, 'Llamar a Fran sin fijarme sus '),
+(22, 8, 'Cuidar a Memilittt'),
+(23, 8, 'Hacer tistos'),
+(24, 8, 'Crear un mundo en Minecraft'),
+(25, 8, 'Eliminar un mundo en Minecraft');
 
 -- --------------------------------------------------------
 
@@ -107,7 +139,30 @@ CREATE TABLE `tareas` (
   `id_subcategoria` int(10) NOT NULL,
   `turno` varchar(20) DEFAULT NULL,
   `estado` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tareas`
+--
+
+INSERT INTO `tareas` (`id_tarea`, `id_usuario`, `id_subcategoria`, `turno`, `estado`) VALUES
+(1, 21, 16, 'Tarde', 'PENDIENTE'),
+(6, 21, 8, 'Mediodía', 'PENDIENTE'),
+(7, 26, 5, 'Mañana', 'PENDIENTE'),
+(8, 27, 1, 'Mañana', 'PENDIENTE'),
+(9, 27, 3, 'Mediodía', 'PENDIENTE'),
+(10, 26, 11, 'Noche', 'PENDIENTE'),
+(11, 26, 13, 'Noche', 'PENDIENTE'),
+(12, 14, 8, 'Tarde', 'PENDIENTE'),
+(13, 14, 8, 'Tarde', 'PENDIENTE'),
+(14, 14, 8, 'Tarde', 'PENDIENTE'),
+(15, 14, 8, 'Tarde', 'PENDIENTE'),
+(16, 14, 8, 'Tarde', 'PENDIENTE'),
+(17, 14, 8, 'Tarde', 'PENDIENTE'),
+(18, 14, 1, 'Mañana', 'PENDIENTE'),
+(19, 28, 8, 'Tarde', 'PENDIENTE'),
+(20, 30, 15, 'Noche', 'PENDIENTE'),
+(21, 30, 14, 'Mediodía', 'PENDIENTE');
 
 -- --------------------------------------------------------
 
@@ -119,16 +174,26 @@ CREATE TABLE `usuarios` (
   `id_usuario` int(10) NOT NULL,
   `nombre_usuario` varchar(20) NOT NULL,
   `correo_usuario` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `correo_usuario`) VALUES
-(1, 'ASD', 'asld@gmail.com'),
-(2, 'LOLETE', 'LOELTE@GMAIL.COM'),
-(3, 'asdasdasd', 'LOELTasdE@GMAIL.COM');
+(11, 'lolete', 'pepe_joestar@gmail.c'),
+(14, 'asd', 'asd2@gmail.com'),
+(16, 'lolete334', 'pepe_joestar@gmail.c'),
+(17, 'papu', 'doupapu@gmail.com'),
+(18, 'Sebita00', 'loelteSeba@gmail.com'),
+(19, 'Lolete00', 'asds2@gmail.com'),
+(20, 'Lolete02', 'asds5@gmail.com'),
+(21, 'Lolete011', 'asds52@gmail.com'),
+(26, 'Sebita', 'pepe_joestar@gmail.c'),
+(27, 'Saitam23a', 'pepe_joestar@gmail.c'),
+(28, 'Lunita', 'lunitaBrother@gmail.'),
+(29, 'Sebita01', 'seba.fernandez.laula'),
+(30, 'Sebita02', 'seba.fernandez.laula');
 
 -- --------------------------------------------------------
 
@@ -164,12 +229,6 @@ ALTER TABLE `respuestas`
   ADD KEY `id_pregunta` (`id_pregunta`);
 
 --
--- Indices de la tabla `rutina_asd`
---
-ALTER TABLE `rutina_asd`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
 -- Indices de la tabla `subcategorias`
 --
 ALTER TABLE `subcategorias`
@@ -198,7 +257,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_categoria` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `preguntas`
@@ -216,19 +275,19 @@ ALTER TABLE `respuestas`
 -- AUTO_INCREMENT de la tabla `subcategorias`
 --
 ALTER TABLE `subcategorias`
-  MODIFY `id_subcategoria` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_subcategoria` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `tareas`
 --
 ALTER TABLE `tareas`
-  MODIFY `id_tarea` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tarea` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_usuario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Restricciones para tablas volcadas
